2f0b915e7565a21ef11839eaef92013943087362
chevy645 <57452581+chevy645@users.noreply.github.com>
2021-09-26 11:48:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -33,35 +33,6 @@ def index():
            transcript = recognizer.recognize_google(data, key=None)
            print(transcript)
 
-
-           # # 1. Ex 1 local media file
-           #
-           # media_file_name_mp3 = 'jobs.mp3'
-           #
-           # with open(media_file_name_mp3, 'rb') as f1:
-           #     byte_data_mp3 = f1.read()
-           # audio_mp3 = speech.RecognitionAudio(content=byte_data_mp3)
-           #
-           # ## 2. Configure Media files
-           #
-           # config_mp3 = speech.RecognitionConfig(
-           #     sample_rate_hertz=48000,
-           #     enable_automatic_punctuation=True,
-           #     language_code='en-US'
-           # )
-           #
-           # # 3. Transcibing the RecognitionAudio objects
-           # # needed to enable Google Cloud to Speech module on website
-           #
-           # response_standard_mp3 = speech_client.recognize(
-           #     config=config_mp3,
-           #     audio=audio_mp3
-           # )
-           #
-           # # print(response_standard_mp3)
-           # transcript = response_standard_mp3.results[0].alternatives[0].transcript
-           print(transcript)
-
    return render_template('index.html', transcript = transcript)
 
 if __name__ == "__main__":
